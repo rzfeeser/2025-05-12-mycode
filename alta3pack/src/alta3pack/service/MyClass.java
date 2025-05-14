aacd782e597eae1f7a8c24be453c650e075b2d7c
RZFeeser <RZFeeser@gmail.com>
2025-05-14 02:13:41 +0000

    coding styles

---
+++ alta3pack/src/alta3pack/service/MyClass.java
@@ -0,0 +1,12 @@
+/* Alta3 Research – alta3pack/service/MyClass.java 
+ * Building a package */
+
+// alta3pack (folder)
+// service   (folder)
+package alta3pack.service;
+
+public class MyClass {
+  public static void javaCow() {   // javaCow is a static function with MyClass
+    System.out.println("java cow goes MOOOOOOO!");
+  }
+}
